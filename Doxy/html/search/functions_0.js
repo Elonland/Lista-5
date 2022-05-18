67f129a8d29c8767ccff341b2738d9cbc5eb3e0a
Elon_Land <33283599+Elonland@users.noreply.github.com>
2022-05-18 11:43:49 +0200

    Add files via upload

---
+++ Doxy/html/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['changestate_0',['changeState',['../class_change_state.html#a7412de59220f07bc53260b5464cd1155',1,'ChangeState']]],
+  ['coordinates_1',['Coordinates',['../class_circle_fx.html#a4e76ccfd2e27e2ec259b1ee228d09e21',1,'CircleFx.Coordinates()'],['../class_rectangle___new.html#a07403ec299557a04e5658e5fe0334788',1,'Rectangle_New.Coordinates()'],['../class_triangle_fx.html#aa5d6cd484ccf25688351a7c8632e6f75',1,'TriangleFx.Coordinates()']]]
+];
---
+++ Figure.java
@@ -0,0 +1,7 @@
+import javafx.scene.Group;
+
+public interface Figure
+{
+    void drawFigure(Group root);
+    void Coordinates(double x, double y);
+}
